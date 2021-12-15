8e8b1437118863708fcc7943ddd1ab12469ef021
askepen <aske@vhg26.dk>
2021-12-15 09:52:45 +0100

    add data module args in method

--- src/data_module.py
+++ src/data_module.py
@@ -1,4 +1,6 @@
 import os
+
+from pytorch_lightning.utilities.argparse import add_argparse_args
 import transforms
 from torch import nn
 from pytorch_lightning import LightningDataModule
@@ -9,17 +11,28 @@ from torch.utils.data import DataLoader, random_split
 class CompressedAudioDataModule(LightningDataModule):
     """PyTorch-Lightning data module for the compressed audio dataset"""
 
-    def __init__(self, data_dir, batch_size, n_fft, stft_width, stft_height, **kwargs):
+    def __init__(self, data_dir, batch_size, num_workers, n_fft, stft_width, stft_height, **kwargs):
         super().__init__()
         self.data_dir = data_dir
         self.batch_size = batch_size
-        self.num_workers = os.cpu_count()
+        self.num_workers = num_workers
         self.transform = nn.Sequential(
             transforms.RandomSubsample(),
             transforms.STFT(n_fft),
             transforms.PadToSize([stft_height, stft_width]),
             transforms.ViewAsReal(),
         )
+
+    @staticmethod
+    def add_argparse_args(parent_parser):
+        parser = parent_parser.add_argument_group("CompressedAudioDataModule")
+        parser.add_argument("--data_dir", type=str, default="./data")
+        parser.add_argument("--batch_size", type=int, default=16)
+        parser.add_argument("--num_workers", type=int, default=os.cpu_count())
+        parser.add_argument("--n_fft", type=int, default=None)
+        parser.add_argument("--stft_width", type=int, default=285)
+        parser.add_argument("--stft_height", type=int, default=751)
+        return parent_parser
 
     def prepare_data(self):
         """Makes sure we have downloaded and processed the data. Is only called once and on 1 GPU"""
@@ -36,7 +49,8 @@ class CompressedAudioDataModule(LightningDataModule):
                 self.data_dir, train=True, transform=self.transform
             )
             lengths = [round(x * len(dataset_train)) for x in [0.25, 0.75]]
-            self.dataset_train, self.dataset_val = random_split(dataset_train, lengths)
+            self.dataset_train, self.dataset_val = random_split(
+                dataset_train, lengths)
         if stage == "test" or stage is None:
             self.dataset_test = CompressedAudioDataset(
                 self.data_dir, train=False, transform=self.transform
--- src/train.py
+++ src/train.py
@@ -18,19 +18,16 @@ def train_from_dict(hparams, trainer_hparams):
 
 def train_from_argparse():
     parser = ArgumentParser()
-    parser.add_argument("--data_dir", type=str, default="./data")
-    parser.add_argument("--batch_size", type=int, default=16)
-    parser.add_argument("--n_fft", type=int, default=None)
-    parser.add_argument("--stft_width", type=int, default=285)
-    parser.add_argument("--stft_height", type=int, default=751)
     parser.add_argument("--wandb", type=bool, default=False)
     parser = Trainer.add_argparse_args(parser)
+    parser = CompressedAudioDataModule.add_argparse_args(parser)
     parser = LitModel.add_model_specific_args(parser)
     args = parser.parse_args()
 
     model = LitModel(**vars(args))
     logger = WandbLogger(project="Bachelor") if args.wandb else None
-    data_module = CompressedAudioDataModule.from_argparse_args(args)
+    data_module = CompressedAudioDataModule.from_argparse_args(
+        args, num_workers=args.num_workers)
     trainer = pl.Trainer.from_argparse_args(args, logger=logger)
     trainer.fit(model, data_module)
 
