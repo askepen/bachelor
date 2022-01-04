ae70c8557eb3ea2d7a38580cceba7e702de273ca
askepen <aske@vhg26.dk>
2022-01-04 14:26:56 +0100

    Amend

--- src/models/fullyconnected.py
+++ src/models/fullyconnected.py
@@ -10,8 +10,8 @@ class RMSLELoss(nn.Module):
         super().__init__()
         self.mse = nn.MSELoss()
 
-    def forward(self, pred, y):
-        return torch.sqrt(self.mse(torch.log(pred + 1), torch.log(y + 1)))
+    def forward(self, pred, actual):
+        return torch.sqrt(self.mse(torch.log(pred + 1), torch.log(actual + 1)))
 
 
 class LitFullyConnected(pl.LightningModule):
