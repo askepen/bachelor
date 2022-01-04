b7c59777205742100242cca82e7cfdb1f277440b
askepen <aske@vhg26.dk>
2022-01-04 19:55:18 +0100

    rmsle loss

--- src/models/unet.py
+++ src/models/unet.py
@@ -36,8 +36,8 @@ class LitUnet(pl.LightningModule):
         self.kernel_size = kernel_size
         self.betas = (b1, b2)
 
-        self.loss_fn = nn.MSELoss()
-        # self.loss_fn = loss.RMSLELoss()
+        # self.loss_fn = nn.MSELoss()
+        self.loss_fn = loss.RMSLELoss()
         self.down = MaxPool2d(2, ceil_mode=True)
         self.down_blocks = torch.nn.ModuleList([
             self.block(in_channels, 64, 0),
